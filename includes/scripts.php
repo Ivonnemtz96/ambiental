ee1db6760dd5d90f9cff82868692bf38ec5fbbfe
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-10-26 12:08:38 -0600

    Se realizan las página sprincipales
    Se crean los includes e index de inicio

---
+++ includes/scripts.php
@@ -0,0 +1,14 @@
+<!-- JavaScript -->
+<script data-cfasync="false" src="https://demo.bosathemes.com/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/vendors/jquery/jquery.js"></script>
+<script src="assets/vendors/waypoint/jquery.waypoints.min.js"></script>
+<script src="assets/vendors/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/vendors/progressbar-fill-visible/js/progressBar.min.js"></script>
+<script src="assets/vendors/jquery-ui/jquery-ui.min.js"></script>
+<script src="assets/vendors/countdown-date-loop-counter/loopcounter.js"></script>
+<script src="assets/vendors/counterup/jquery.counterup.js"></script>
+<script src="assets/vendors/modal-video/jquery-modal-video.min.js"></script>
+<script src="assets/vendors/masonry/masonry.pkgd.min.js"></script>
+<script src="assets/vendors/fancybox/dist/jquery.fancybox.min.js"></script>
+<script src="assets/vendors/slick/slick.min.js"></script>
+<script src="assets/vendors/slick-nav/jquery.slicknav.js"></script>
+<script src="assets/js/custom.js"></script>
