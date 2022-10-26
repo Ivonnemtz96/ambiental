c26421288751d3e665b5f23c98b456de4cc12755
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-10-26 16:51:39 -0600

    Se agregan valores en página nosotros y se agrega pt-4

---
+++ modules/nosotros.php
@@ -0,0 +1,279 @@
+<main id="content" class="site-main">
+    <!-- Inner Banner html start-->
+    <section class="inner-banner-wrap">
+        <div class="inner-baner-container" style="background-image: url(assets/images/inner-banner-img-1.jpg);">
+            <div class="container">
+                <div class="inner-banner-content">
+                    <h1 class="inner-title">Sobre Nosotros</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Inner Banner html end-->
+    <!-- About page html start -->
+    <section class="about-page-section">
+        <div class="container">
+            <div class="section-head">
+                <div class="row align-items-center justify-content-center">
+                    <h2 class="section-title text-center">¿Quiénes somos?</h2>
+                    <div class="col-lg-6">
+                        <!-- <div class="back-title">INTRODUCTION</div> -->
+
+                        <!-- <span
+                                class="primary-color">
+                                <svg class="title-shape" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 150"
+                                    preserveAspectRatio="none">
+                                    <path
+                                        d="M9.3,127.3c49.3-3,150.7-7.6,199.7-7.4c121.9,0.4,189.9,0.4,282.3,7.2C380.1,129.6,181.2,130.6,70,139 c82.6-2.9,254.2-1,335.9,1.3c-56,1.4-137.2-0.3-197.1,9">
+                                    </path>
+                                </svg>
+                            </span> -->
+                        <img src="assets/images/img7.jpg" alt="not found">
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="section-disc">
+                            <p>
+                                Somos una organización enfocada a brindar servicios de consultoría ambiental orientados
+                                a
+                                organismos públicos, particulares y empresas privadas. Conformada por un grupo de
+                                profesionales
+                                y especialistas en materia de ecología y ciencias afines con el propósito común de
+                                generar un
+                                equilibrio entre el desarrollo de la sociedad y el ambiente.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row h-100 justify-content-center g-3">
+                    <h2 class="section-title text-center">
+                        Nuestros <span class="primary-color"> Valores
+                            <svg class="title-shape" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 150"
+                                preserveAspectRatio="none">
+                                <path
+                                    d="M9.3,127.3c49.3-3,150.7-7.6,199.7-7.4c121.9,0.4,189.9,0.4,282.3,7.2C380.1,129.6,181.2,130.6,70,139 c82.6-2.9,254.2-1,335.9,1.3c-56,1.4-137.2-0.3-197.1,9">
+                                </path>
+                            </svg>
+                        </span>
+                    </h2>
+                    <div class="col-md-4 comprometidos text-center">
+                        <div class="grid blog-inner row">
+                            <div class="grid-item">
+                                <article class="post">
+                                    <figure class="pt-4">
+                                            <img src="assets/images/icons/planeta.png" alt="">
+                                    </figure>
+                                    <hr>
+                                    <div class="entry-content">
+                                        <h4>Comprometidos con el ambiente</h4>
+                                        <p>
+                                            Los proyectos y actividades se realizan de forma respetuosa con
+                                            la conservación del ambiente, desde la perspectiva del cuidado del
+                                            entorno natural y el
+                                            desarrollo
+                                            sostenible. Esta empresa está comprometida con la calidad de vida de la
+                                            sociedad actual y de
+                                            las
+                                            generaciones futuras y sus operaciones se realizan de forma responsable
+                                            y eficiente con los
+                                            recursos naturales.
+                                        </p>
+                                    </div>
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 calidad text-center">
+                        <div class="grid blog-inner row">
+                            <div class="grid-item">
+                                <article class="post">
+                                    <figure class="pt-4">
+                                        <a href="blog-single.html">
+                                            <img src="assets/images/icons/calidad.png" alt="">
+                                        </a>
+                                    </figure>
+                                    <hr>
+                                    <div class="entry-content">
+                                        <h4>Profesionales de calidad</h4>
+                                        <p>
+                                            Esta empresa cuenta con un equipo de profesionales de alta calificación
+                                            y experiencia tanto en lo laboral como en lo académico, teniendo más de
+                                            5 años ejerciendo la
+                                            consultoría ambiental y 10 años dedicados a la investigación.
+                                            <br><br><br><br><br><br>
+                                        </p>
+                                    </div>
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 valores text-center">
+                        <div class="grid blog-inner row">
+                            <div class="grid-item">
+                                <article class="post">
+                                    <figure class="pt-4">
+                                        <a href="blog-single.html">
+                                            <img src="assets/images/icons/innovation.png" alt="">
+                                        </a>
+                                    </figure>
+                                    <hr>
+                                    <div class="entry-content">
+                                        <h4>Innovación tecnológica</h4>
+                                        <p>
+                                            Somos una empresa que representa el asesoramiento y la consultoría
+                                            ambiental, así como el de innovar en nuevos productos y servicios basados en
+                                            los últimos
+                                            avances
+                                            tecnológicos. Usamos las tecnologías de información geográfica GIS y GPS.
+                                            Nos apoyamos en
+                                            los
+                                            nuevos medios de comunicación, divulgación y marketing: redes sociales y los
+                                            canales de
+                                            difusión
+                                            web, para conseguir llegar a más gente y ofrecer un producto con mayor
+                                            repercusión social.
+                                        </p>
+                                    </div>
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div hidden class="iconbox-container-bg">
+                <div class="iconbox-item-bg">
+                    <div class="iconbox-content-bg">
+                        <i aria-hidden="true" class="fas fa-smog"></i>
+                        <h4>No Pollution</h4>
+                    </div>
+                </div>
+                <div class="iconbox-item-bg">
+                    <div class="iconbox-content-bg">
+                        <i aria-hidden="true" class="icon icon-Leaf"></i>
+                        <h4>Afforstation</h4>
+                    </div>
+                </div>
+                <div class="iconbox-item-bg">
+                    <div class="iconbox-content-bg">
+                        <i aria-hidden="true" class="fas fa-fill-drip"></i>
+                        <h4>Clean water</h4>
+                    </div>
+                </div>
+                <div class="iconbox-item-bg">
+                    <div class="iconbox-content-bg">
+                        <i aria-hidden="true" class="fas fa-heart"></i>
+                        <h4>Good health</h4>
+                    </div>
+                </div>
+                <div class="iconbox-item-bg">
+                    <div class="iconbox-content-bg">
+                        <i aria-hidden="true" class="fas fa-piggy-bank"></i>
+                        <h4>Animal Habitat</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="about-video-wrap">
+                <div class="about-video-content">
+                    <h3>Visión</h3>
+                    <p>
+                        Ser una empresa a nivel nacional e internacional en la aplicación de estrategias que
+                        contribuyan al desarrollo sustentable y económico.
+                    </p>
+                </div>
+                <div class="video-image" style="background-image: url(assets/images/img4.jpg);">
+                    <!-- <div class="video-button">
+                        <a id="video-container" data-video-id="IUN664s7N-c">
+                            <i class="fas fa-play"></i>
+                        </a>
+                    </div> -->
+                    <div class="overlay"></div>
+                </div>
+            </div>
+
+            <div class="about-video-wrap mt-5">
+                <div class="video-image" style="background-image: url(assets/images/img5.jpg);">
+                    <div class="overlay"></div>
+                </div>
+                <div class="about-video-content" style="margin-left:-50px ;">
+                    <h3>Misión</h3>
+                    <p>
+                        Resolver problemas ambientales del sector industrial, de servicios y gubernamental
+                        aplicando conocimientos, habilidades y técnicas para proveer servicios especializados, creando
+                        así
+                        relaciones de confianza con nuestros clientes y asegurando su completa satisfacción.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <!-- client section html start -->
+        <div class="client-section">
+            <div class="container">
+                <div class="client-wrap client-slider text-center">
+                    <div class="client-item">
+                        <figure>
+                            <img src="assets/images/logo1.png" alt="">
+                        </figure>
+                    </div>
+                    <div class="client-item">
+                        <figure>
+                            <img src="assets/images/logo2.png" alt="">
+                        </figure>
+                    </div>
+                    <div class="client-item">
+                        <figure>
+                            <img src="assets/images/logo3.png" alt="">
+                        </figure>
+                    </div>
+                    <div class="client-item">
+                        <figure>
+                            <img src="assets/images/logo4.png" alt="">
+                        </figure>
+                    </div>
+                    <div class="client-item">
+                        <figure>
+                            <img src="assets/images/logo5.png" alt="">
+                        </figure>
+                    </div>
+                    <div class="client-item">
+                        <figure>
+                            <img src="assets/images/logo2.png" alt="">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- client html end -->
+        <!-- home callback section html start -->
+        <div class="callback-inner-page" style="background-image: url(assets/images/banner-img.jpg);">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 offset-lg-2">
+                        <div class="callback-content text-center">
+                            <div class="section-head">
+                                <div class="back-title">INVOLVE NOW</div>
+                                <h2 class="section-title">We've Funfed 13,500 Chairt Projects For 25M People Arpund
+                                    The
+                                    <span class="primary-color">World !
+                                        <svg class="title-shape" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 500 150" preserveAspectRatio="none">
+                                            <path
+                                                d="M9.3,127.3c49.3-3,150.7-7.6,199.7-7.4c121.9,0.4,189.9,0.4,282.3,7.2C380.1,129.6,181.2,130.6,70,139 c82.6-2.9,254.2-1,335.9,1.3c-56,1.4-137.2-0.3-197.1,9">
+                                            </path>
+                                        </svg>
+                                    </span>
+                                </h2>
+                            </div>
+                            <div class="button-wrap">
+                                <a href="volunteer.html" class="button-round-primary">Get Involve Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="overlay"></div>
+        </div>
+        <!-- home callback section html end -->
+    </section>
+    <!-- About page html end -->
+</main>
