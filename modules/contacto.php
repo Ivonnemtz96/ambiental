8020d931feada675da8d8b70483af98dfe312ddf
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-10-27 11:22:12 -0600

    Servicios terminado

---
+++ modules/contacto.php
@@ -0,0 +1,152 @@
+<main id="content" class="site-main">
+    <!-- Inner Banner html start-->
+    <section class="inner-banner-wrap">
+        <div class="inner-baner-container" style="background-image: url(assets/images/inner-banner-img-1.jpg);">
+            <div class="container">
+                <div class="inner-banner-content">
+                    <h1 class="inner-title">Contáctanos</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Inner Banner html end-->
+    <!-- contact form html start -->
+    <div class="contact-page-section">
+        <div class="contact-form-inner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="contact-detail-container">
+                            <div class="section-head">
+                                <div class="back-title">CONTACT US!</div>
+                                <h2 class="section-title">Feel Free To Contact & <span class="primary-color">Reach Us!
+                                        <svg class="title-shape" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 500 150" preserveAspectRatio="none">
+                                            <path
+                                                d="M9.3,127.3c49.3-3,150.7-7.6,199.7-7.4c121.9,0.4,189.9,0.4,282.3,7.2C380.1,129.6,181.2,130.6,70,139 c82.6-2.9,254.2-1,335.9,1.3c-56,1.4-137.2-0.3-197.1,9">
+                                            </path>
+                                        </svg>
+                                    </span>
+                                </h2>
+                                <div class="section-disc">
+                                    <p>Praesent consequatur aptent, tincidunt habitant numquam, laoreet fringilla
+                                        consequatur provident, interdum anim quas dolorum sed. Egestas debitis eleifend
+                                        eniam repudiandae.</p>
+                                </div>
+                            </div>
+                            <div class="contact-details-list">
+                                <ul>
+                                    <li>
+                                        <span class="icon">
+                                            <i class="fas fa-map-marker-alt"></i>
+                                        </span>
+                                        <div class="details-content">
+                                            <h4>Head office address :</h4>
+                                            <span>4671 Sugar Camp Road, Rock Street, San Francisco, United States
+                                                America</span>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <span class="icon">
+                                            <i class="fas fa-phone-volume"></i>
+                                        </span>
+                                        <div class="details-content">
+                                            <h4>Toll free number :</h4>
+                                            <span>Mobile-no: 507-754-2294 // 205-234-5590 Phone-no: +(113) 526 255 //
+                                                +(254) 255 699</span>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <span class="icon">
+                                            <i class="fas fa-envelope-open-text"></i>
+                                        </span>
+                                        <div class="details-content">
+                                            <h4>Email address :</h4>
+                                            <span>Website-url: www.example_1234.com // Email-link: <a
+                                                    href="https://demo.bosathemes.com/cdn-cgi/l/email-protection"
+                                                    class="__cf_email__"
+                                                    data-cfemail="432a2d252c03272c2e222a2d6d202c2e">[email&#160;protected]</a></span>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="contact-from-wrap" style="background-image: url(assets/images/banner-img.jpg);">
+                            <form class="contact-from">
+                                <p>
+                                    <input type="text" name="name" placeholder="Enter Name*">
+                                </p>
+                                <p>
+                                    <input type="email" name="email" placeholder="Enter Email*">
+                                </p>
+                                <p>
+                                    <input type="text" name="subject" placeholder="Enter Subject*">
+                                </p>
+                                <p>
+                                    <input type="number" name="name" placeholder="Enter Number*">
+                                </p>
+                                <p class="width-full">
+                                    <textarea rows="8" placeholder="Enter Message*"></textarea>
+                                </p>
+                                <p class="width-full">
+                                    <input type="submit" name="submit" value="Submit Now">
+                                </p>
+                            </form>
+                            <div class="overlay"></div>
+                        </div>
+                        <div class="footer-social-icon">
+                            <h4>Follow us on social media..</h4>
+                            <br />
+                            <div class="social-icon-wrap">
+                                <div class="social-icon social-facebook">
+                                    <a href="#" target="_blank">
+                                        <i class="fab fa-facebook-f"></i>
+                                        <span>Facebook</span>
+                                    </a>
+                                </div>
+                                <div class="social-icon social-pinterest">
+                                    <a href="#" target="_blank">
+                                        <i class="fab fa-pinterest"></i>
+                                        <span>Pinterest</span>
+                                    </a>
+                                </div>
+                                <div class="social-icon social-whatsapp">
+                                    <a href="#" target="_blank">
+                                        <i class="fab fa-whatsapp"></i>
+                                        <span>WhatsApp</span>
+                                    </a>
+                                </div>
+                                <div class="social-icon social-linkedin">
+                                    <a href="#" target="_blank">
+                                        <i class="fab fa-linkedin"></i>
+                                        <span>Linkedin</span>
+                                    </a>
+                                </div>
+                                <div class="social-icon social-twitter">
+                                    <a href="#" target="_blank">
+                                        <i class="fab fa-twitter"></i>
+                                        <span>Twitter</span>
+                                    </a>
+                                </div>
+                                <div class="social-icon social-google">
+                                    <a href="#" target="_blank">
+                                        <i class="fab fa-google-plus-g"></i>
+                                        <span>Google</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="map-section">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d317838.95217734354!2d-0.27362819527326965!3d51.51107287614788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487604c7c7eb9be3%3A0x3918653583725b56!2sRiverside%20Building%2C%20County%20Hall%2C%20Westminster%20Bridge%20Rd%2C%20London%20SE1%207JA%2C%20UK!5e0!3m2!1sen!2snp!4v1632135241093!5m2!1sen!2snp"
+                height="400" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+    </div>
+    <!-- contact form html end -->
+</main>
---
+++ modules/servicios.php
@@ -0,0 +1,200 @@
+<main id="content" class="site-main">
+    <!-- Inner Banner html start-->
+    <section class="inner-banner-wrap bg-light-grey">
+        <div class="inner-baner-container" style="background-image: url(assets/images/inner-banner-img-1.jpg);">
+            <div class="container">
+                <div class="inner-banner-content">
+                    <h1 class="inner-title">Nuestros servicios</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Inner Banner html end-->
+    <div class="charity-page-section bg-light-grey">
+        <div class="charity-page-inner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <article class="charity-item">
+                            <figure class="charity-image">
+                                <img src="assets/images/img12.jpg" alt="">
+                                <!-- <div class="btn-wrap">
+                                    <a href="donate.html" class="button-round-primary">DONATE NOW</a>
+                                </div> -->
+                            </figure>
+                            <div class="charity-content">
+                                <h4>Autorizaciones y permisos en materia de recursos forestales y suelos</h4>
+                                <p>
+                                <ul>
+                                    <li>Cambio de uso de suelo en terrenos forestales:
+                                        <h6>Si quieres construir dentro
+                                            de zonas
+                                            forestales del país y eso implica una remoción total o parcial de la
+                                            vegetación
+                                            forestal para actividades no forestales, debes solicitar el cambio de uso de
+                                            suelo
+                                            ante la Secretaría de Medio Ambiente y Recursos Naturales (SEMARNAT).</h6>
+                                    </li>
+                                    <li>Informes de cumplimiento</li>
+                                    <li>Recolección Germoplasma Forestal.</li>
+                                    <li>
+                                        Autorización de Aprovechamiento de Recursos Forestales Maderables en Terrenos
+                                        Forestales o Preferentemente Forestales. Modalidad A. En predios particulares.
+                                    </li>
+                                    <li>
+                                        Autorización de aprovechamiento de recursos forestales maderables en terrenos
+                                        forestales o preferentemente forestales.
+                                    </li>
+                                    <li>Aviso de aprovechamiento forestal no maderable.</li>
+                                    <li>Informe anual de aprovechamiento forestal.</li>
+                                    <li>
+                                        Autorización para Adelantar el Plan de Corta, Alterar el Calendario Aprobado o
+                                        Modificar el Programa de Manejo Forestal, sin modalidades.
+                                    </li>
+                                    <li>Remisiones forestales para legal procedencia.</li>
+                                    <li>
+                                        Remisiones forestales para legal procedencia; Modalidad B: Tramites
+                                        subsecuentes.
+                                    </li>
+                                    <li>Modificar autorización centros almacenamiento y transformación.</li>
+                                    <li>Autorización de centro de almacenamiento y transformación.</li>
+                                </ul>
+                                </p>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-md-6">
+                        <article class="charity-item">
+                            <figure class="charity-image">
+                                <img src="assets/images/img13.jpg" alt="">
+                                <!-- <div class="btn-wrap">
+                                    <a href="donate.html" class="button-round-primary">DONATE NOW</a>
+                                </div> -->
+                            </figure>
+                            <div class="charity-content">
+                                <h4>Autorizaciones y permisos en materia de impacto ambiental</h4>
+                                <p>
+                                    <ul>
+                                        <li>Informe Preventivo</li>
+                                        <li>MIA Particular</li>
+                                        <li>MIA Particular con Riesgo</li>
+                                        <li>MIA Regional</li>
+                                        <li>MIA Regional con Riesgo</li>
+                                        <li>Aviso de desistimiento</li>
+                                        <li>Solicitud de exención</li>
+                                        <li>Aviso de no requerimiento de autorización</li>
+                                        <li>Aviso de cambio de titularidad</li>
+                                        <li>Trámite unificado cambio de uso de suelo</li>
+                                        <li>Programas de supervisión</li>
+                                        <li>Informes de cumplimiento</li>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <br>
+
+                                    </ul>
+                                </p>
+                               
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-md-6">
+                        <article class="charity-item">
+                            <figure class="charity-image">
+                                <img src="assets/images/img14.jpg" alt="">
+                                <!-- <div class="btn-wrap">
+                                    <a href="donate.html" class="button-round-primary">DONATE NOW</a>
+                                </div> -->
+                            </figure>
+                            <div class="charity-content">
+                                <h4>Autorizaciones y permisos en materia de materiales y actividades riesgosas</h4>
+                                <p>
+                                    <ul>
+                                        <li>Estudio de riesgo</li>
+                                        <li>Programa de prevención de accidentes</li>
+                                    </ul>
+                                </p>
+
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-md-6">
+                        <article class="charity-item">
+                            <figure class="charity-image">
+                                <img src="assets/images/img14.jpg" alt="">
+                                <!-- <div class="btn-wrap">
+                                    <a href="donate.html" class="button-round-primary">DONATE NOW</a>
+                                </div> -->
+                            </figure>
+                            <div class="charity-content">
+                                <h4>Autorizaciones y permisos en materia de vida silvestre</h4>
+                                <p>
+                                    <ul>
+                                        <li>Registro como PIMVS o UMA</li>
+                                        <li>Informes anuales de PIMVS</li>
+                                    </ul>
+                                </p>
+
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-md-6">
+                        <article class="charity-item">
+                            <figure class="charity-image">
+                                <img src="assets/images/img14.jpg" alt="">
+                                <!-- <div class="btn-wrap">
+                                    <a href="donate.html" class="button-round-primary">DONATE NOW</a>
+                                </div> -->
+                            </figure>
+                            <div class="charity-content">
+                                <h4>Autorizaciones y permisos en materia de residuos peligrosos</h4>
+                                <p>
+                                    <ul>
+                                        <li>Registro generador de residuos peligrosos</li>
+                                        <li>Informe de reciclaje</li>
+                                        <li>Alta y categorización de residuos peligrosos</li>
+                                        <li>Registro del plan de manejo de residuos peligrosos</li>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <br>
+                                    </ul>
+                                </p>
+
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-md-6">
+                        <article class="charity-item">
+                            <figure class="charity-image">
+                                <img src="assets/images/img7.jpg" alt="">
+                            </figure>
+                            <div class="charity-content">
+                                <h4>Otras autorizaciones y permisos en materia de vida silvestre</h4>
+                                <p>
+                                    <ul>
+                                        <li>Caracterización de flora</li>
+                                        <li>Registro como generador de residuos de manejo especial</li>
+                                        <li>Respuestas y/o seguimiento a visitas de inspección para cumplimiento ambiental</li>
+                                        <li>Pre-auditoría ambiental</li>
+                                        <li>Licencia municipal de construcción</li>
+                                        <li>Licencia de construcción por término de obra</li>
+                                        <li>Factibilidad de agua potable y servicios</li>
+                                        <li>Licencia de funcionamiento mercantil</li>
+                                    </ul>
+                                </p>
+                                
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
