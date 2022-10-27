05297d7385fc19dc8668d96d59333be614b3a92f
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-10-27 11:37:59 -0600

    contacto terminado

---
+++ contacto.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$title = 'Inicio';
+$inicio = 'active';
+include('includes/head.php');
+?>
+
+<body class="home">
+    <?php 
+    include('includes/preloader.php')
+    ?>
+    <div id="page" class="full-page">
+        <?php
+        include('includes/header.php');
+        include('modules/contacto.php');
+        include('includes/footer.php');
+        ?>
+    </div>
+    <?php 
+    include('includes/scripts.php');
+    ?>
+
+</body>
+
+</html>
--- modules/contacto.php
+++ modules/contacto.php
@@ -18,8 +18,8 @@
                     <div class="col-md-6">
                         <div class="contact-detail-container">
                             <div class="section-head">
-                                <div class="back-title">CONTACT US!</div>
-                                <h2 class="section-title">Feel Free To Contact & <span class="primary-color">Reach Us!
+                                <h2 class="section-title">Si tienes dudas o preguntas <span
+                                        class="primary-color">¡Contáctanos!
                                         <svg class="title-shape" xmlns="http://www.w3.org/2000/svg"
                                             viewBox="0 0 500 150" preserveAspectRatio="none">
                                             <path
@@ -41,9 +41,10 @@
                                             <i class="fas fa-map-marker-alt"></i>
                                         </span>
                                         <div class="details-content">
-                                            <h4>Head office address :</h4>
-                                            <span>4671 Sugar Camp Road, Rock Street, San Francisco, United States
-                                                America</span>
+                                            <h4>Dirección:</h4>
+                                            <span>
+                                                San José del Cabo
+                                            </span>
                                         </div>
                                     </li>
                                     <li>
@@ -51,9 +52,13 @@
                                             <i class="fas fa-phone-volume"></i>
                                         </span>
                                         <div class="details-content">
-                                            <h4>Toll free number :</h4>
-                                            <span>Mobile-no: 507-754-2294 // 205-234-5590 Phone-no: +(113) 526 255 //
-                                                +(254) 255 699</span>
+                                            <h4>Teléfono:</h4>
+                                            <span>
+                                                <a style="color: black;text-decoration: underline;"
+                                                    href="tel:+521234567890">
+                                                    +52 123 456 7890
+                                                </a>
+                                            </span>
                                         </div>
                                     </li>
                                     <li>
@@ -61,11 +66,13 @@
                                             <i class="fas fa-envelope-open-text"></i>
                                         </span>
                                         <div class="details-content">
-                                            <h4>Email address :</h4>
-                                            <span>Website-url: www.example_1234.com // Email-link: <a
-                                                    href="https://demo.bosathemes.com/cdn-cgi/l/email-protection"
-                                                    class="__cf_email__"
-                                                    data-cfemail="432a2d252c03272c2e222a2d6d202c2e">[email&#160;protected]</a></span>
+                                            <h4>Correo:</h4>
+                                            <span>
+                                                <a style="color: black;text-decoration: underline;"
+                                                    href="mailto:info@ambiente.com">
+                                                    info@ambiente.com
+                                                </a>
+                                            </span>
                                         </div>
                                     </li>
                                 </ul>
@@ -76,28 +83,28 @@
                         <div class="contact-from-wrap" style="background-image: url(assets/images/banner-img.jpg);">
                             <form class="contact-from">
                                 <p>
-                                    <input type="text" name="name" placeholder="Enter Name*">
+                                    <input type="text" name="name" placeholder="Nombre*">
                                 </p>
                                 <p>
-                                    <input type="email" name="email" placeholder="Enter Email*">
+                                    <input type="email" name="email" placeholder="Correo*">
                                 </p>
                                 <p>
-                                    <input type="text" name="subject" placeholder="Enter Subject*">
+                                    <input type="text" name="tel" placeholder="Teléfono*">
                                 </p>
                                 <p>
-                                    <input type="number" name="name" placeholder="Enter Number*">
+                                    <input type="number" name="name" placeholder="Asunto*">
                                 </p>
                                 <p class="width-full">
-                                    <textarea rows="8" placeholder="Enter Message*"></textarea>
+                                    <textarea rows="8" placeholder="Mensaje*"></textarea>
                                 </p>
                                 <p class="width-full">
-                                    <input type="submit" name="submit" value="Submit Now">
+                                    <input type="submit" name="submit" value="Enviar">
                                 </p>
                             </form>
                             <div class="overlay"></div>
                         </div>
                         <div class="footer-social-icon">
-                            <h4>Follow us on social media..</h4>
+                            <h4>¡Síguenos en redes sociales!</h4>
                             <br />
                             <div class="social-icon-wrap">
                                 <div class="social-icon social-facebook">
@@ -106,36 +113,36 @@
                                         <span>Facebook</span>
                                     </a>
                                 </div>
-                                <div class="social-icon social-pinterest">
+                                <!-- <div class="social-icon social-pinterest">
                                     <a href="#" target="_blank">
                                         <i class="fab fa-pinterest"></i>
                                         <span>Pinterest</span>
                                     </a>
-                                </div>
+                                </div> -->
                                 <div class="social-icon social-whatsapp">
                                     <a href="#" target="_blank">
                                         <i class="fab fa-whatsapp"></i>
                                         <span>WhatsApp</span>
                                     </a>
                                 </div>
-                                <div class="social-icon social-linkedin">
+                                <!-- <div class="social-icon social-linkedin">
                                     <a href="#" target="_blank">
                                         <i class="fab fa-linkedin"></i>
                                         <span>Linkedin</span>
                                     </a>
-                                </div>
+                                </div> -->
                                 <div class="social-icon social-twitter">
                                     <a href="#" target="_blank">
-                                        <i class="fab fa-twitter"></i>
-                                        <span>Twitter</span>
+                                        <i class="fab fa-instagram"></i>
+                                        <span>Instagram</span>
                                     </a>
                                 </div>
-                                <div class="social-icon social-google">
+                                <!-- <div class="social-icon social-google">
                                     <a href="#" target="_blank">
                                         <i class="fab fa-google-plus-g"></i>
                                         <span>Google</span>
                                     </a>
-                                </div>
+                                </div> -->
                             </div>
                         </div>
                     </div>
@@ -143,9 +150,7 @@
             </div>
         </div>
         <div class="map-section">
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d317838.95217734354!2d-0.27362819527326965!3d51.51107287614788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487604c7c7eb9be3%3A0x3918653583725b56!2sRiverside%20Building%2C%20County%20Hall%2C%20Westminster%20Bridge%20Rd%2C%20London%20SE1%207JA%2C%20UK!5e0!3m2!1sen!2snp!4v1632135241093!5m2!1sen!2snp"
-                height="400" allowfullscreen="" loading="lazy"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d757.803220471923!2d-109.70833038815377!3d23.09021394731968!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86af51532c7132c7%3A0xf09c9724da511bbd!2sBanana%20Marketing%20Group!5e0!3m2!1ses-419!2smx!4v1666892201465!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
     <!-- contact form html end -->
