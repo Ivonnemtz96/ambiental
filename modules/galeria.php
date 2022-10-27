a1304673eae9cced5f00631632bd9b6ea47e1b74
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-10-27 10:36:05 -0600

    galería creada

---
+++ modules/galeria.php
@@ -0,0 +1,78 @@
+<main id="content" class="site-main">
+    <!-- Inner Banner html start-->
+    <section class="inner-banner-wrap">
+        <div class="inner-baner-container" style="background-image: url(assets/images/inner-banner-img-1.jpg);">
+            <div class="container">
+                <div class="inner-banner-content">
+                    <h1 class="inner-title">Nuestra Galería</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Inner Banner html end-->
+    <!-- gallery section html start -->
+    <section class="gallery-page-section gallery-section">
+        <div class="container">
+            <div class="gallery-inner">
+                <div class="gallery-container grid">
+                    <div class="single-gallery grid-item">
+                        <figure class="gallery-img">
+                            <a href="assets/images/gallery-img1.jpg" data-fancybox="gallery">
+                                <img src="assets/images/gallery-img1.jpg" alt="">
+                            </a>
+                        </figure>
+                    </div>
+                    <div class="single-gallery grid-item">
+                        <figure class="gallery-img">
+                            <a href="assets/images/gallery-img2.jpg" data-fancybox="gallery">
+                                <img src="assets/images/gallery-img2.jpg" alt="">
+                            </a>
+                        </figure>
+                    </div>
+                    <div class="single-gallery grid-item">
+                        <figure class="gallery-img">
+                            <a href="assets/images/gallery-img3.jpg" data-fancybox="gallery">
+                                <img src="assets/images/gallery-img3.jpg" alt="">
+                            </a>
+                        </figure>
+                    </div>
+                    <div class="single-gallery grid-item">
+                        <figure class="gallery-img">
+                            <a href="assets/images/img2.jpg" data-fancybox="gallery">
+                                <img src="assets/images/img2.jpg" alt="">
+                            </a>
+                        </figure>
+                    </div>
+                    <div class="single-gallery grid-item">
+                        <figure class="gallery-img">
+                            <a href="assets/images/gallery-img4.jpg" data-fancybox="gallery">
+                                <img src="assets/images/gallery-img4.jpg" alt="">
+                            </a>
+                        </figure>
+                    </div>
+                    <div class="single-gallery grid-item">
+                        <figure class="gallery-img">
+                            <a href="assets/images/gallery-img5.jpg" data-fancybox="gallery">
+                                <img src="assets/images/gallery-img5.jpg" alt="">
+                            </a>
+                        </figure>
+                    </div>
+                    <div class="single-gallery grid-item">
+                        <figure class="gallery-img">
+                            <a href="assets/images/gallery-img6.jpg" data-fancybox="gallery">
+                                <img src="assets/images/gallery-img6.jpg" alt="">
+                            </a>
+                        </figure>
+                    </div>
+                    <div class="single-gallery grid-item">
+                        <figure class="gallery-img">
+                            <a href="assets/images/gallery-img7.jpg" data-fancybox="gallery">
+                                <img src="assets/images/gallery-img7.jpg" alt="">
+                            </a>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
