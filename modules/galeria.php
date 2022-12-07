ecd488d502ed70f78d2ca367e3890815fabe0236
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-12-07 16:12:13 -0700

    cambio de fotos de portada en pestañas

--- modules/galeria.php
+++ modules/galeria.php
@@ -1,7 +1,7 @@
 <main id="content" class="site-main">
     <!-- Inner Banner html start-->
     <section class="inner-banner-wrap">
-        <div class="inner-baner-container" style="background-image: url(assets/images/inner-banner-img-1.jpg);">
+        <div class="inner-baner-container" style="background-image: url(assets/images/inner-banner-img-3.jpg);">
             <div class="container">
                 <div class="inner-banner-content">
                     <h1 class="inner-title">Nuestra Galería</h1>
--- modules/servicios.php
+++ modules/servicios.php
@@ -1,7 +1,7 @@
 <main id="content" class="site-main">
     <!-- Inner Banner html start-->
     <section class="inner-banner-wrap bg-light-grey">
-        <div class="inner-baner-container" style="background-image: url(assets/images/inner-banner-img-1.jpg);">
+        <div class="inner-baner-container" style="background-image: url(assets/images/inner-banner-img-4.jpg);">
             <div class="container">
                 <div class="inner-banner-content">
                     <h1 class="inner-title">Nuestros servicios</h1>
